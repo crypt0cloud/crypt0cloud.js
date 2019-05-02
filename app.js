0d588c6d3c38705c4b40c4d3c258d575b72ac8a6
Jose Angel <jose@ngel.es>
2019-05-02 00:55:39 -0500

    post single transaction

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ var crypt0 = require('./index.js');
 
 let cli = new crypt0.Client("crypt0cloud-demo.appspot.com");
 
+/*
 cli.getNodeId()
+    .then(function (obj) {
+        console.log(obj);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+cli.getCurrentBlock()
+    .then(function (obj) {
+        console.log(obj);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+*/
+let cc = new crypt0.Cloud("crypt0cloud-demo.appspot.com")
+let unique = (new Date).getTime().toString();
+cc.CreateUser(unique)
     .then(function (obj) {
         console.log(obj);
     })
--- index.js
+++ index.js
@@ -2,15 +2,48 @@ var rp = require('request-promise');
 var nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-var sha256 = require("js-sha256");
+var sha256 = require("js-sha256").sha256;
 
 exports.printMsg = function() {
     console.log("This is a message from the demo package");
 }
 
+class CryptoCloud{
+    constructor(endpoint){
+        this.client = new Client(endpoint);
+        this.cry = new Crypto();
+    }
+
+    async CreateUser(user) {
+        let b1 = await this.client.getCurrentBlock();
+        let nid = await this.client.getNodeId();
+
+        let kp = this.cry.NewKeypair();
+
+        let t = {
+            AppID: this.cry.Base64Encode(kp.publicKey),
+            Payload: user,
+            SignKind: "NewUser",
+            SignerKinds: ["NewUser"],
+            FromNode: nid,
+            ToNode: nid,
+
+            Callback: "demo",
+            Creation: this.cry.CurrentMilis(),
+            BlockSign: b1.Sign,
+        };
+        t.Signer = t.AppID
+
+        await this.client.PostSingleTransaction(t,kp);
+
+        return kp;
+    }
+}
+
 class Client{
     constructor(edp){
         this._endpoint = edp;
+        this.cry = new Crypto();
     }
 
     getNodeId(){
@@ -26,19 +59,16 @@ class Client{
             })
         });
 
-
-
-
         return ;
     }
 
-    getCurrentBlockAsync(){
+    getCurrentBlock(){
         if (this._endpoint == ""){
             return null;
         }
 
         return new Promise(resolve => {
-            let urlstring = "https://" + this._endpoint + "/api/v1/block/get_last";
+            let urlstring = "https://" + this._endpoint + "/api/v1/block/get_lasts";
             rp(urlstring).then(htmlString => {
                 let arr = JSON.parse(htmlString)
                 resolve(arr[0]);
@@ -51,13 +81,79 @@ class Client{
             return null;
         }
 
-        let cry = new Crypto();
-        t = cry.signTransaction(t,keys);
+        t = this.cry.signTransaction(t,keys);
 
         return new Promise(resolve => {
-            let urlstring = "https://" + this._endpoint + "/api/v1/post_single_transaction";
-            rp(urlstring).then(htmlString => {
-                let obj = JSON.parse(htmlString)
+            let options = {
+              method: 'POST',
+              uri: "https://" + this._endpoint + "/api/v1/post_single_transaction",
+              body: t,
+                json:true,
+                headers:{
+                  'Content-Type': 'application/json'
+                },
+            };
+
+            console.log(options);
+
+            rp(options).then(obj => {
+                console.log(obj);
+                resolve(obj);
+            })
+        });
+    }
+
+    CreateGroup(t, keys){
+        t = this.cry.signTransaction(t,keys);
+
+        return new Promise(resolve => {
+            let options = {
+                method: 'POST',
+                uri: "https://" + this._endpoint + "/api/v1/create_group",
+                body: t,
+                json: true
+            };
+
+            rp(options).then(obj => {
+                resolve(obj);
+            })
+        });
+    }
+
+    CreateSigningRequest(t){
+        return new Promise(resolve => {
+            let options = {
+                method: 'POST',
+                uri: "https://" + this._endpoint + "/api/v1/create_signingRequest",
+                body: t,
+                json: true
+            };
+
+            rp(options).then(obj => {
+                resolve(obj);
+            })
+        });
+    }
+    GetSigningRequest(transactionrequest){
+        return new Promise(resolve => {
+            let urlstring = "https://" + this._endpoint + "/api/v1/get_signingRequest?id=".concat(transactionrequest.toString());
+            rp(urlstring).then(obj => {
+                resolve(obj);
+            })
+        });
+    }
+    SignSigningRequest(t,keys){
+        t = this.cry.signTransaction(t,keys);
+
+        return new Promise(resolve => {
+            let options = {
+                method: 'POST',
+                uri: "https://" + this._endpoint + "/api/v1/sign_signingRequest",
+                body: t,
+                json: true
+            };
+
+            rp(options).then(obj => {
                 resolve(obj);
             })
         });
@@ -70,14 +166,26 @@ class Crypto{
         let content = nacl.util.decodeUTF8(jsonstr);
         t.Content = this.Base64Encode(content);
 
-        t.Hash = this.Base64Encode(sha256(content))
-        t.Sign = this.Base64Encode(nacl.sign.detached(content, key.secretKet));
+        let hash = sha256.array(content);
+        t.Hash = this.Base64Encode(hash)
+        t.Sign = this.Base64Encode(nacl.sign.detached(this.Base64Decode(t.Hash), keys.secretKey));
 
-        t.Signer = this.Base64Encode(key.publicKey);
+        t.Signer = this.Base64Encode(keys.publicKey);
+        return t;
     }
-    Base64Encode(plaintext){
-        return nacl.util.encodeBase64(plaintext);
+    Base64Encode(bytearray){
+        return nacl.util.encodeBase64(bytearray);
+    }
+    Base64Decode(str){
+        return nacl.util.decodeBase64(str)
+    }
+    NewKeypair(){
+        return nacl.sign.keyPair();
+    }
+    CurrentMilis(){
+        return (new Date).getTime();
     }
 }
 
 exports.Client = Client;
+exports.Cloud = CryptoCloud;
