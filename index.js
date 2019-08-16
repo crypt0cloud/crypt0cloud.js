539a1ed8b831cb96fa7532c52d27eaa937819ead
Jose Angel Espinoza Portillo <jose@ngel.es>
2019-08-16 00:47:10 -0500

    working

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+var rp = require('request-promise');
+var ed25519 = require('ed25519');
+
 exports.printMsg = function() {
     console.log("This is a message from the demo package");
 }
+
+class Client{
+    constructor(edp){
+        this._endpoint = edp;
+    }
+
+    getNodeId(){
+        if (this._endpoint == ""){
+            return null;
+        }
+
+        return new Promise(resolve => {
+            let urlstring = "https://" + this._endpoint + "/api/v1/node_id";
+            rp(urlstring).then(htmlString => {
+                let obj = JSON.parse(htmlString)
+                resolve(obj);
+            })
+        });
+
+
+
+
+        return ;
+    }
+}
+
+exports.Client = Client;
